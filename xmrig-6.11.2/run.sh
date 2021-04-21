019cd611f5479806943dd42ad82c8b360742263c
Fan Young <ya1995fatw@gmail.com>
2021-04-20 18:51:48 -0700

    add xmrig

---
+++ xmrig-6.11.2/run.sh
@@ -0,0 +1,9 @@
+./xmrig \
+    -o rx.unmineable.com:3333 \
+    -a rx \
+    -k \
+    -u ADA:addr1q889mu4kx790zpkddgpux9sae4grad0vwugkkrnlvrlzesxwthetvdu27yrv66srcvtpmn2s8667cac3dv887c879nqq89wlaj.vps1
+
+
+
+
